72545a984c671ebf3b0699048a0e6416fb960ef0
UntoldGam <charliebird373@gmail.com>
2022-09-16 22:36:55 +0100

    1

--- routes/economy.js
+++ routes/economy.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = express.Router(); // same as app
+const db = require("../firebase.js");
 
 router.get("/", (req, res) => {
     res.status(200).json({ "status": "working correctly" })
 });
 
-router.get("/getData/:id", (req, res) => {
-    const db = require("../firebase.js");
+router.get("/:id/get", (req, res) => {
 
     let ref = db.ref(`/${req.params.id}/leaderstats`)
     ref.on("value", async (snapshot) => {
         res.send(snapshot.val())
     })
+
+})
+
+router.get("/:id/set", (req, res) => {
+    let data = req.query.data; // the table that the user's data should be changed to
+
+    let ref = db.ref(`/${req.params.id}/leaderstats`)
+    ref.set(req.query.data)
 
 })
 
