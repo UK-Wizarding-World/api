9fd28d20457699adbbdf2da260d0ca92a28da9bc
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2022-09-16 15:09:37 +0100

    update

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ const cors = require("cors");
 app.use(cors())
 
 app.get("/", (req, res) => {
-
+    res.status(200).json({ "message": "api online" })
 })
+
+
+const listener = app.listen(process.env.PORT || 5000, (listener) => {
+    console.log("listening on port: "+listener.address().port)
+})
+
